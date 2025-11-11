84185b61bb2c196afa982539219d41d6c8dbfc63
Julia Moraes <jliamoraesmartins01@gmail.com>
2025-11-10 21:47:58 -0300

    Confirmado

--- professorData.js
+++ professorData.js
@@ -1,36 +1,3 @@
-let professores = [
-    {
-    id: "1",
-    nome: "José Vasques",
-    idade: 46,
-    departamento: "Química",
-    turmas: [
-    { codigo: "3A", disciplina: "Quimica1", alunos: ["João Vitor", "Anna Clara", "Pedro Henrique"] },
-    { codigo: "3B", disciplina: "Quimica2", alunos: ["Mariana Mendes", "Gustavo Henrique", "Caique Fadel"] }
-    ]
-},
-    {
-        id: "2",
-        nome: "Natalia Valente",
-        idade: 41,
-        departamento: "Geografia",
-        turmas: [
-            { codigo: "2A", disciplina: "Geografia1", alunos: ["Julia Moraes", "Julia Lamare", "Luiza Coffani"] },
-            { codigo: "2B", disciplina: "Geografia2", alunos: ["Thifany Antunes", "Carolina Freitas", "Camila Souza"] }
-        ]
-    },
-
-    {id: "3",
-    nome: "Donizete Silva",
-    idade: 49,
-    departamento: "Matemática",
-    turmas: [
-        { codigo: "1A", disciplina: "Matematica1", alunos: ["Vitoria Freitas", "Mateus Oliveira", "Rafaela Souza"] },
-        { codigo: "1B", disciplina: "Matematica2", alunos: ["Danielle Namie", "Melissa Yukari", "Gabriel Oliveira"] }
-        ]
-    }
-];
-
 app.get('/professores', (req, res) => {
     res.json(professores);
 });
